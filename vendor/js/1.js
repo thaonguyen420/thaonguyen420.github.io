b49b377780e08637ea186a91dd821aff53e795cd
Thaonguyen <thaonguyen@gmail.com>
2019-12-04 18:12:39 +0700

    lan

--- vendor/js/1.js
+++ vendor/js/1.js
@@ -1,4 +1,10 @@
-﻿ $(function(){
- 
-})  
- 
+$(function() {
+    locationofgl = $('#grade-levels').offset().top;
+    console.log(locationofgl);
+
+    $(".mouse").click(function() {
+        console.log('dduwocj di');
+        $('body, html').animate({ scrollTop: locationofgl });
+        return false;
+    })
+})
